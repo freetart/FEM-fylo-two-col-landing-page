3792182d23c72d048f5fe59fc604cee8e6432755
habib <habibdev96@gmail.com>
2021-06-08 16:42:43 -0600

    added bg pattern & adjusted spacing between sections

--- dist/glide.483e68ef.js
+++ dist/glide.483e68ef.js
@@ -3825,7 +3825,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56906" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51589" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
